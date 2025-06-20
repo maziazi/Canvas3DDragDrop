caf1dd6b60c6f58cb494900f7d87b931a47fbf53
maziazi <mazis9651@gmail.com>
2025-06-21 02:32:22 +0700

    Initial Commit

---
+++ Canvas3DDragDropTests/Canvas3DDragDropTests.swift
@@ -0,0 +1,17 @@
+//
+//  Canvas3DDragDropTests.swift
+//  Canvas3DDragDropTests
+//
+//  Created by Muhamad Azis on 21/06/25.
+//
+
+import Testing
+@testable import Canvas3DDragDrop
+
+struct Canvas3DDragDropTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
